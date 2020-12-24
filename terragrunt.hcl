8b23b00fd8ae2128f5172412ba212cdd14635c8c
coloradoe <rxdelper@gmail.com>
2020-12-24 09:53:16 -0600

    Fix the subdirectory workspace creation

--- terragrunt.hcl
+++ terragrunt.hcl
@@ -1,5 +1,5 @@
 locals {
-  aws_region = "us-east-2"
+  aws_region = "us-west-2"
 }
 
 remote_state {
--- terraworkspace.sh
+++ terraworkspace.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-usage() {                                      
+usage() {
 cat << EOF
-usage: bash ./terraworkspace.sh -r workspace_name 
+usage: bash ./terraworkspace.sh -r workspace_name
 -r    | --workspace_name (Required)  Workspace name must match [us-east-1| us-east-2| us-west-1 | us-west-2]
 -h    | --help                       Brings up this menu
 EOF
 }
-# 
+#
 # Display the usage message if no arguments are supplied
 #
 if [ $# -eq 0 ]
@@ -17,7 +17,6 @@ fi
 
 while getopts r:h:*: flag
 do
-    echo ${flag}
     case "${flag}" in
     r)
        region=${OPTARG}
@@ -37,6 +36,14 @@ do
            terraform workspace new "$region"
            # Create workspace in all subdirectories
            #for d in ./*/ ; do (cd "$d" && terraform workspace new "$region");done
+           for d in */;
+           do(
+               # ignore terra dirs and hidden dirs/files
+               if [[ "$d" == *"terra"* ]] || [[ "$d" =~ ^\. ]]; then
+                   continue
+               fi
+           # Create workspace in each module directory
+           cd "$d" && terraform workspace new "$region"); done
        fi
 
        if [[ " ${array[*]} " =~ " $region " ]];
@@ -45,7 +52,15 @@ do
            # Select the workspace
            terraform workspace select "$region"
            # Select workspace in all subdirectories
-           for d in ./*/ ; do (cd "$d" && terraform workspace select "$region");done
+           #for d in ./*/ ; do (cd "$d" && terraform workspace select "$region");done
+           for d in */;
+            do(
+                # ignore terra dirs and hidden dirs/files
+                if [[ "$d" == *"terra"* ]] || [[ "$d" =~ ^\. ]]; then
+                    continue
+                fi
+            # Select workspace in each module directory
+            cd "$d" && terraform workspace select "$region"); done
        fi
 
        # Set region in terragrunt file
