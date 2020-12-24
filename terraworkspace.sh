1a0e82f6dbb142cb83ee5f738c5aa1bc7845eb42
Bob <bob.burke@beyondidentity.com>
2020-12-23 23:01:06 -0600

    test on dir search

--- terraworkspace.sh
+++ terraworkspace.sh
@@ -1,52 +1,65 @@
 #!/bin/bash
+usage() {                                      
+cat << EOF
+usage: bash ./terraworkspace.sh -r workspace_name 
+-r    | --workspace_name (Required)  Workspace name must match [us-east-1| us-east-2| us-west-1 | us-west-2]
+-h    | --help                       Brings up this menu
+EOF
+}
+# 
+# Display the usage message if no arguments are supplied
+#
+if [ $# -eq 0 ]
+then
+    usage
+    exit
+fi
 
-# Get the region
 while getopts r:h:*: flag
 do
+    echo ${flag}
     case "${flag}" in
-        r)
-            region=${OPTARG}
-            array=()
+    r)
+       region=${OPTARG}
+       array=()
 
-            for i in $(terraform workspace list| sed 's/*//g')
-            do
-                array+=("$i")
-                echo "array[$i]"
-            done
+       for i in $(terraform workspace list| sed 's/*//g')
+       do
+           array+=("$i")
+           echo "array[$i]"
+       done
+       echo "exiting"
 
-            if [[ ! " ${array[*]} " =~ " $region " ]];
-            then
-                echo "Creating $region"
-                # Create workspace if workspace doesn't exist
-                terraform workspace new "$region"
-                # Create workspace in all subdirectories
-                for d in ./*/ ; do (cd "$d" && terraform workspace new "$region");done
-            fi
+       if [[ ! " ${array[*]} " =~ " $region " ]];
+       then
+           echo "Creating $region"
+           # Create workspace if workspace doesn't exist
+           terraform workspace new "$region"
+           # Create workspace in all subdirectories
+           #for d in ./*/ ; do (cd "$d" && terraform workspace new "$region");done
+       fi
 
-            if [[ " ${array[*]} " =~ " $region " ]];
-            then
-                echo "Selecting $region"
-                # Select the workspace
-                terraform workspace select "$region"
-                # Select workspace in all subdirectories
-                for d in ./*/ ; do (cd "$d" && terraform workspace select "$region");done
-            fi
+       if [[ " ${array[*]} " =~ " $region " ]];
+       then
+           echo "Selecting $region"
+           # Select the workspace
+           terraform workspace select "$region"
+           # Select workspace in all subdirectories
+           for d in ./*/ ; do (cd "$d" && terraform workspace select "$region");done
+       fi
 
-        # Set region in terragrunt file
-        hclq set 'locals.aws_region' "$region" < terragrunt.hcl | sponge terragrunt.hcl
-        printf "Region Set to:\n"
-        hclq get 'locals.aws_region' < terragrunt.hcl
-        ;;
+       # Set region in terragrunt file
+       hclq set 'locals.aws_region' "$region" < terragrunt.hcl | sponge terragrunt.hcl
+       printf "Region Set to:\n"
+       hclq get 'locals.aws_region' < terragrunt.hcl
+       ;;
     h)
-        echo "Terraworkspace changes the terraform workspace of the current and subdirectories"
-        echo " "
-        echo "Usage: ./terraworkspace.sh [options]"
-        echo " "
-        echo "Options:"
-        echo "-help                show help"
-        echo "-r                   specify a region to create or select a workspace"
-        ;;
+       echo "h stage"
+       ;;
     *)
+       usage
+       exit
+       ;;
 esac
 done
 
